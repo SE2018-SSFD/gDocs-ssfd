8ff390124197751a51f1cd54c8640902abccf95d
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2021-06-12 21:26:09 +0800

    fix(backend): fix one2many relationship for entity sheet and checkpoint By kanzhen Wu

--- code/Backend/model/File.go
+++ code/Backend/model/File.go
@@ -4,10 +4,11 @@ import "time"
 
 type Sheet struct {
 	Fid			uint			`gorm:"primaryKey;AUTOINCREMENT=1" json:"fid"`
-	IsDeleted	bool			`gorm:"type:TINYINT(1);default:0" json:"isDeleted"`
+	IsDeleted	bool			`gorm:"type:TINYINT;default:0" json:"isDeleted"`
 	Name		string			`gorm:"type:VARCHAR(100)" json:"name"`
 	CheckPoints	[]CheckPoint	`json:"checkpoints"`
 	Path		string			`gorm:"type:VARCHAR(200)" json:"-"`
+	Users		[]User			`gorm:"many2many:users_sheets" json:"-"`
 	Columns		int				`json:"columns"`
 
 	Content		[]string		`gorm:"-" json:"content"`
@@ -15,6 +16,7 @@ type Sheet struct {
 
 type CheckPoint struct {
 	Cid			uint			`gorm:"primaryKey;AUTOINCREMENT=1" json:"cid"`
+	SheetID		uint			`json:"-"`
 	CreatedAt	time.Time
 	Columns		uint			`json:"columns"`
 	Path		string			`gorm:"type:VARCHAR(200)" json:"-"`
