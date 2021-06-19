fc995f4d774991da1a3a87c1e6afb9c5b23bcffb
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2021-06-19 21:34:01 +0800

    feat(backend): add common wrapper functions for dfs By Kanzhen Wu

--- code/Backend/dao/DfsDao.go
+++ code/Backend/dao/DfsDao.go
@@ -3,12 +3,12 @@ package dao
 import (
 	"backend/dfs"
 	"backend/utils"
+	"errors"
 	"fmt"
 )
 
 
-// TODO: Find a Way to Get the Real Size of Cell to Fix Bugs of FileInsert etc.
-func writeAll(fd int, off int64, content string) error {
+func writeAll(fd int, off int64, content string) (err error) {
 	toWrite := int64(len(content))
 	for toWrite > 0 {
 		n, err := dfs.Write(fd, off, content[:toWrite])
@@ -18,7 +18,7 @@ func writeAll(fd int, off int64, content string) error {
 		toWrite = toWrite - n
 		off = off + n
 	}
-
+	
 	if toWrite != 0 {
 		return fmt.Errorf("expect to write %d bytes, actually it is %d", len(content),
 			int64(len(content)) - toWrite)
@@ -27,77 +27,156 @@ func writeAll(fd int, off int64, content string) error {
 	return nil
 }
 
-func FileCreate(path string) error {
+func FileCreate(path string, initSize int64) (err error) {
 	fd, err := dfs.Create(path)
 	if err != nil {
 		return err
 	}
 
-	err = writeAll(fd, 0, string(utils.Zeros(utils.CELL_SIZE)))
+	if initSize != 0 {
+		err = writeAll(fd, 0, string(utils.Zeros(initSize)))
+		if err != nil {
+			return err
+		}
+	}
 
+	err = dfs.Close(fd)
 	return err
 }
 
-func FileInsert(path string, off int64, content string, maxsize int64) error {
+func FileGetAll(path string) (content string, err error) {
+	fileInfo, err := dfs.Stat(path)
+	if err != nil {
+		return "", err
+	}
+
+	length := fileInfo.Size
+	fd, err := dfs.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	content, err = dfs.Read(fd, 0, length)
+	return content, err
+}
+
+func FileAppend(path string, content string) (err error) {
+	fileInfo, err := dfs.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir {
+		return errors.New("cannot append to a directory")
+	}
+
+	off := fileInfo.Size
+	fd, err := dfs.Open(path)
+	if err != nil {
+		return err
+	}
+
+	err = writeAll(fd, off, content)
+	if err != nil {
+		return err
+	}
+
+	err = dfs.Close(fd)
+	return err
+}
+
+func FileOverwriteAll(path string, content string) error {
+	fileInfo, err := dfs.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir {
+		return errors.New("cannot write a directory")
+	}
+
+	fd, err := dfs.Open(path)
+	if err != nil {
+		return err
+	}
+
 	toWrite := int64(len(content))
-	if off + toWrite > maxsize {
-		toWrite = maxsize - off
+	if fileInfo.Size > toWrite {
+		err = dfs.Truncate(fd, toWrite)
 	}
-
-	fd, err := dfs.Open(path)
 	if err != nil {
 		return err
 	}
 
-	block, err := dfs.Read(fd, off, toWrite)
+	err = writeAll(fd, 0, content)
 	if err != nil {
 		return err
 	}
 
-	block = block[:off] + content + block[off:]
-	err = writeAll(fd, off, block[:toWrite])
-
+	err = dfs.Close(fd)
 	return err
 }
 
-func FileDelete(path string, off int64, length int64, maxsize int64) error {
-	fd, err := dfs.Open(path)
-	if err != nil {
-		return err
-	}
-
-	block, err := dfs.Read(fd, off, length)
-	if err != nil {
-		return err
-	}
-
-	if off + length > maxsize {
-		length = maxsize - off
-	}
-	block = block[:off] + block[off + length:]
-	err = writeAll(fd, off, block)
-
-	return err
-}
-
-func FileOverwrite(path string, off int64, content string, maxsize int64) error {
-	toWrite := int64(len(content))
-	fd, err := dfs.Open(path)
-	if err != nil {
-		return err
-	}
-
-	block, err := dfs.Read(fd, off, toWrite)
-	if err != nil {
-		return err
-	}
-
-	originSize := int64(len(block))
-	block = block[:off] + content + block[off:]
-	if toWrite + originSize > maxsize {
-		toWrite = maxsize - originSize
-	}
-	err = writeAll(fd, off, block[:toWrite])
-
-	return err
-}
+//func FileInsert(path string, off int64, content string, maxsize int64) error {
+//	toWrite := int64(len(content))
+//	if off + toWrite > maxsize {
+//		toWrite = maxsize - off
+//	}
+//
+//	fd, err := dfs.Open(path)
+//	if err != nil {
+//		return err
+//	}
+//
+//	block, err := dfs.Read(fd, off, toWrite)
+//	if err != nil {
+//		return err
+//	}
+//
+//	block = block[:off] + content + block[off:]
+//	err = writeAll(fd, off, block[:toWrite])
+//
+//	return err
+//}
+//
+//func FileDelete(path string, off int64, length int64, maxsize int64) error {
+//	fd, err := dfs.Open(path)
+//	if err != nil {
+//		return err
+//	}
+//
+//	block, err := dfs.Read(fd, off, length)
+//	if err != nil {
+//		return err
+//	}
+//
+//	if off + length > maxsize {
+//		length = maxsize - off
+//	}
+//	block = block[:off] + block[off + length:]
+//	err = writeAll(fd, off, block)
+//
+//	return err
+//}
+//
+//func FileOverwrite(path string, off int64, content string, maxsize int64) error {
+//	toWrite := int64(len(content))
+//	fd, err := dfs.Open(path)
+//	if err != nil {
+//		return err
+//	}
+//
+//	block, err := dfs.Read(fd, off, toWrite)
+//	if err != nil {
+//		return err
+//	}
+//
+//	originSize := int64(len(block))
+//	block = block[:off] + content + block[off:]
+//	if toWrite + originSize > maxsize {
+//		toWrite = maxsize - originSize
+//	}
+//	err = writeAll(fd, off, block[:toWrite])
+//
+//	return err
+//}
