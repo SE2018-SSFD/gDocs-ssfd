6a2c9cd7fd331f1aef5e335b503ef703cba6329b
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2021-07-08 11:07:43 +0800

    test(backend): add concurrency test for backend By Kanzhen Wu

--- code/Backend/tests/concurrency_test/base_test.go
+++ code/Backend/tests/concurrency_test/base_test.go
@@ -1,22 +1,45 @@
 package concurrency_test
 
 import (
+	"backend/utils"
+	"backend/utils/logger"
+	"bytes"
+	"encoding/json"
+	"github.com/kataras/golog"
+	"github.com/sacOO7/gowebsocket"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
 
+const (
+	wsApi = "sheetws"
+)
+
 var (
 	hosts = []string{
-		"localhost:10086",
-		"localhost:10087",
-		"localhost:10088",
+		"192.168.1.107:10086",
+		"192.168.1.107:10087",
+		"192.168.1.107:10088",
+	}
+
+	loginParams = []utils.LoginParams{
+		{"test", "test"},
+		{"test1", "test1"},
+		{"test2", "test2"},
 	}
 )
 
 func TestMain(m *testing.M) {
 	rand.Seed(time.Now().UnixNano())
+	logger.SetLogger(golog.New())
+	logger.SetLevel("Info")
 
 	exitCode := m.Run()
 	os.Exit(exitCode)
@@ -25,4 +48,324 @@ func TestMain(m *testing.M) {
 func randomHost() string {
 	idx := rand.Int() % 3
 	return hosts[idx]
+}
+
+func randomHostHttp() string {
+	idx := rand.Int() % 3
+	return "http://" + hosts[idx]
+}
+
+func get(addr string, api string, params string, respBody interface{}) (err error) {
+	url := addr + "/" + api + "?" + params
+
+	logger.Debugf("[%s] Send Get: %s", url, params)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	respBodyRaw, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	respBodyRaw = TransEscape(respBodyRaw)
+
+	if respBody != nil {
+		err = json.Unmarshal(respBodyRaw, respBody)
+		if err != nil {
+			return err
+		}
+	}
+
+	logger.Debugf("[%s] Get Json Response: %v", url, respBody)
+
+	return nil
+}
+
+func getRaw(addr string, api string, params string) (raw []byte, err error) {
+	url := addr + "/" + api + "?" + params
+
+	logger.Debugf("[%s] Send Get: %s", url, params)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	raw, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	raw = TransEscape(raw)
+
+	logger.Debugf("[%s] Get Raw: %s", url, raw)
+	return raw, err
+}
+
+func post(addr string, api string, reqBody interface{}, respBody interface{}) (err error) {
+	url := addr + "/" + api
+	reqBodyRaw, _ := json.Marshal(reqBody)
+
+	logger.Debugf("[%s] Send Post: %s", url, reqBodyRaw)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqBodyRaw))
+	if err != nil {
+		return err
+	}
+
+	respBodyRaw, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	respBodyRaw = TransEscape(respBodyRaw)
+
+	logger.Debugf("[%s] Get Post Raw: %s", url, respBodyRaw)
+
+	if respBody != nil {
+		err = json.Unmarshal(respBodyRaw, respBody)
+		if err != nil {
+			return err
+		}
+	}
+
+	logger.Debugf("[%s] Get Post Json Response: %v", url, respBody)
+
+	return nil
+}
+
+func getPostRet(addr string, api string, reqBody interface{}, respBody interface{}) (err error) {
+	bean := ResponseBean{}
+	err = post(addr, api, reqBody, &bean)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bean.Data, respBody)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getPostRaw(addr string, api string, reqBody interface{}) (raw []byte, err error) {
+	bean := ResponseBean{}
+	err = post(addr, api, reqBody, &bean)
+	if err != nil {
+		return nil, err
+	}
+
+	return bean.Data, nil
+}
+
+func getWSAddr(token string, fid uint) string {
+	bean := ResponseBean{}
+	addr := randomHost()
+	err := get("http://" + addr, wsApi, "fid="+strconv.Itoa(int(fid))+"&token="+token+"&query=1", &bean)
+	if err != nil {
+		return ""
+	}
+	if !bean.Success {
+		str := strings.Trim(string(bean.Data), "\"")
+		logger.Debugf("[%q] Get WS address", str)
+		return str
+	} else {
+		str := "ws://"+addr+"/"+wsApi+"?fid="+strconv.Itoa(int(fid))+"&token="+token
+		logger.Debugf("[%q] Get WS address", str)
+		return str
+	}
+}
+
+func login(t *testing.T, params utils.LoginParams) (token string) {
+	loginRet := LoginRet{}
+	err := getPostRet(randomHostHttp(), "login", params, &loginRet)
+	if assert.NoError(t, err) {
+		return loginRet.Token
+	} else {
+		t.Fatal(err)
+		return ""
+	}
+}
+
+func TransEscape(data []byte) []byte {
+	data = bytes.Replace(data, []byte("\\u0026"), []byte("&"), -1)
+	data = bytes.Replace(data, []byte("\\u003c"), []byte("<"), -1)
+	data = bytes.Replace(data, []byte("\\u003e"), []byte(">"), -1)
+	return data
+}
+
+type onAcquireFunc func(msg sheetPrepareNotify)
+type onModifyFunc func(msg sheetModifyNotify)
+type onReleaseFunc func(msg sheetPrepareNotify)
+type onConnFunc func(msg sheetOnConnNotify)
+
+type myWS struct {
+	ws	gowebsocket.Socket
+}
+
+type sheetMessage struct {
+	MsgType		string				`json:"msgType"`	// acquire, modify, release, onConn
+	Body		json.RawMessage		`json:"body"`
+}
+
+// client -> server
+type sheetAcquireMessage struct {
+	Row			int 		`json:"row"`
+	Col			int			`json:"col"`
+}
+
+type sheetModifyMessage struct {
+	Row			int 			`json:"row"`
+	Col			int				`json:"col"`
+	Content		string			`json:"content"`
+	Info		json.RawMessage	`json:"info"`
+}
+
+type sheetReleaseMessage struct {
+	Row			int 		`json:"row"`
+	Col			int			`json:"col"`
+}
+
+// server -> client
+type sheetPrepareNotify struct {
+	Row			int 		`json:"row"`
+	Col			int			`json:"col"`
+	Username	string		`json:"username"`
+}
+
+type sheetModifyNotify struct {
+	Row			int 			`json:"row"`
+	Col			int				`json:"col"`
+	Content		string			`json:"content"`
+	Info		json.RawMessage	`json:"info"`
+	Username	string			`json:"username"`
+}
+
+type cellLockNotify struct {
+	Row			int
+	Col			int
+	Username	string
+}
+
+type sheetOnConnNotify struct {
+	Name			string				`json:"name"`
+	Columns			int					`json:"columns"`
+	Content			[]string			`json:"content"`
+	CellLocks		[]cellLockNotify	`json:"cellLocks"`
+}
+
+func NewWebSocket(t *testing.T, addr string,
+	func1 onAcquireFunc, func2 onModifyFunc, func3 onReleaseFunc, func4 onConnFunc) (ws *myWS) {
+
+	ws = &myWS{
+		ws: gowebsocket.New(addr),
+	}
+
+	ws.ws.ConnectionOptions = gowebsocket.ConnectionOptions{
+		UseSSL: false,
+		UseCompression: true,
+	}
+
+	t.Log(ws.ws.RequestHeader.Clone())
+
+	ws.ws.OnTextMessage = func(content string, socket gowebsocket.Socket) {
+		data := []byte(content)
+		bean := sheetMessage{}
+		if err := json.Unmarshal(data, &bean); err != nil {
+			t.Error(err)
+		} else {
+			switch bean.MsgType {
+			case "acquire":
+				msg := sheetPrepareNotify{}
+				err = json.Unmarshal(bean.Body, &msg)
+				if err != nil {
+					t.Error(err)
+				} else {
+					func1(msg)
+				}
+			case "modify":
+				msg := sheetModifyNotify{}
+				err = json.Unmarshal(bean.Body, &msg)
+				if err != nil {
+					t.Error(err)
+				} else {
+					func2(msg)
+				}
+			case "release":
+				msg := sheetPrepareNotify{}
+				err = json.Unmarshal(bean.Body, &msg)
+				if err != nil {
+					t.Error(err)
+				} else {
+					func3(msg)
+				}
+			case "onConn":
+				msg := sheetOnConnNotify{}
+				err = json.Unmarshal(bean.Body, &msg)
+				if err != nil {
+					t.Error(err)
+				} else {
+					func4(msg)
+				}
+			default:
+				t.Errorf("[%s] No matched msgType", bean.MsgType)
+			}
+		}
+	}
+	ws.ws.OnConnectError = func(err error, socket gowebsocket.Socket) {
+		t.Error(err)
+	}
+	ws.ws.Connect()
+	return ws
+}
+
+func (ws *myWS) SendJson(msgType string, msg interface{}) (err error) {
+	raw, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	sheetMsg := sheetMessage{
+		MsgType: msgType,
+		Body: raw,
+	}
+
+	raw, err = json.Marshal(sheetMsg)
+	if err != nil {
+		return err
+	}
+
+	ws.ws.SendBinary(raw)
+	return nil
+}
+
+
+type ResponseBean struct {
+	Success		bool			`json:"success"`
+	Msg			int				`json:"msg"`
+	Data		json.RawMessage	`json:"data"`
+}
+
+type LoginRet struct {
+	Info	json.RawMessage		`json:"info"`
+	Token	string				`json:"token"`
+}
+
+
+type GetSheetRet struct {
+	Fid						uint		`json:"fid"`
+	IsDeleted				bool		`json:"isDeleted"`
+	Name					string		`json:"name"`
+	CheckPointNum			int			`json:"checkpoint_num"`
+	Columns					int			`json:"columns"`
+	Owner					string		`json:"owner"`
+
+	CreatedAt 				time.Time	`json:"CreatedAt"`
+	UpdatedAt 				time.Time	`json:"UpdatedAt"`
+
+	Content					[]string	`json:"content"`
 }
--- code/Backend/tests/concurrency_test/correctness_test.go
+++ code/Backend/tests/concurrency_test/correctness_test.go
@@ -1,7 +1,301 @@
-package concurrency
+package concurrency_test
 
-import "testing"
+import (
+	"backend/lib/cache"
+	"backend/utils"
+	"backend/utils/logger"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"strconv"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
 
-func TestRedirect(t *testing.T) {
+func TestSingleFile(t *testing.T) {
+	goTestWorkFlowInterrupt(t, 0)
+}
 
+//func TestMultiFile(t *testing.T) {
+//	testRoomN := 30
+//	wg := sync.WaitGroup{}
+//	wg.Add(testRoomN)
+//	for i := 0; i < testRoomN; i += 1 {
+//		go func() {
+//			goTestWorkFlowInterrupt(t, time.Second)
+//			wg.Done()
+//		}()
+//		time.Sleep(time.Second)
+//	}
+//	wg.Wait()
+//}
+
+func goTestWorkFlowInterrupt(t *testing.T, pause time.Duration) {
+	testWsNum := 20
+	initRow, initCol := 10, 10
+	tokenS, tokenR := login(t, loginParams[0]), login(t, loginParams[1])
+	newSheetArg := utils.NewSheetParams{
+		Token: tokenS,
+		Name:  "test",
+		InitRows: uint(initRow),
+		InitColumns: uint(initCol),
+	}
+	raw, _ := getPostRaw(randomHostHttp(), "newsheet", newSheetArg)
+	fidInt, err := strconv.Atoi(string(raw))
+	fid := uint(fidInt)
+	if assert.NoError(t, err) {
+		getSheetArg := utils.GetSheetParams{
+			Token: tokenR,
+			Fid: fid,
+		}
+		getSheetRet := GetSheetRet{}
+		err = getPostRet(randomHostHttp(), "getsheet", getSheetArg, &getSheetRet)
+		if assert.NoError(t, err) {
+			assert.EqualValues(t, fid, getSheetRet.Fid)
+			assert.EqualValues(t, 0, getSheetRet.CheckPointNum)
+			assert.Equal(t, "test", getSheetRet.Name)
+
+			row, col := len(getSheetRet.Content) / getSheetRet.Columns, getSheetRet.Columns
+			assert.Equal(t, len(getSheetRet.Content), row * col)
+
+			ms := make([]*cache.MemSheet, testWsNum)
+			for i := 0; i < testWsNum; i += 1 {
+				ms[i] = cache.NewMemSheet(initRow, initCol)
+			}
+
+			var msSender, msReceiver *cache.MemSheet
+			var mapS, mapR sync.Map
+
+			type cellKey struct {
+				Row int
+				Col int
+			}
+
+			cnt := 0
+			onAcquireS := func(msg sheetPrepareNotify) {}
+			onModifyS := func(msg sheetModifyNotify) {
+				logger.Info(cnt)
+				cnt++
+				logger.Debugf("[%+v] onModify", msg)
+				msSender.Set(msg.Row, msg.Col, msg.Content)
+				mapS.Store(cellKey{Row: msg.Row, Col: msg.Col}, msg.Content)
+			}
+			onReleaseS := func(msg sheetPrepareNotify) {}
+			onConnS := func(msg sheetOnConnNotify) {
+				logger.Debugf("[%+v] onConnection", msg)
+				msSender = cache.NewMemSheetFromStringSlice(msg.Content, msg.Columns)
+				for i := 0; i < len(msg.Content); i += 1 {
+					mapS.Store(cellKey{Row: i/msg.Columns, Col: i%msg.Columns}, msg.Content[i])
+				}
+			}
+
+			onAcquireR := func(msg sheetPrepareNotify) {}
+			onModifyR := func(msg sheetModifyNotify) {
+				logger.Debugf("[%+v] onModify", msg)
+				msReceiver.Set(msg.Row, msg.Col, msg.Content)
+				mapR.Store(cellKey{Row: msg.Row, Col: msg.Col}, msg.Content)
+			}
+			onReleaseR := func(msg sheetPrepareNotify) {}
+			onConnR := func(msg sheetOnConnNotify) {
+				logger.Debugf("[%+v] onConnection", msg)
+				msReceiver = cache.NewMemSheetFromStringSlice(msg.Content, msg.Columns)
+				for i := 0; i < len(msg.Content); i += 1 {
+					mapR.Store(cellKey{Row: i/msg.Columns, Col: i%msg.Columns}, msg.Content[i])
+				}
+			}
+			wsUrlS, wsUrlR := getWSAddr(tokenS, fid), getWSAddr(tokenR, fid)
+			wsSender := NewWebSocket(t, wsUrlS, onAcquireS, onModifyS, onReleaseS, onConnS)
+
+
+			runWS := func(ws *myWS, stopChan chan int) {
+				for {
+					select {
+					case <- stopChan:
+						return
+					default:
+						row, col := rand.Int()%100, rand.Int()%100
+						content := strings.Repeat("test", rand.Int()%30)
+
+						err := ws.SendJson("modify", sheetModifyMessage{
+							Row:     row,
+							Col:     col,
+							Content: content,
+							Info:    nil,
+						})
+
+						if !assert.NoError(t, err) {
+							break
+						}
+						time.Sleep(pause)
+					}
+				}
+			}
+			stopChanS, stopChanR := make(chan int), make(chan int)
+			go runWS(wsSender, stopChanS)
+			time.Sleep(10 * time.Second)
+			wsReceiver := NewWebSocket(t, wsUrlR, onAcquireR, onModifyR, onReleaseR, onConnR)
+			go runWS(wsReceiver, stopChanR)
+			time.Sleep(30 * time.Second)
+			stopChanS <- 1
+			stopChanR <- 1
+			time.Sleep(15 * time.Second)
+			ssS, ssR := msSender.ToStringSlice(), msReceiver.ToStringSlice()
+			colS, rowS := msSender.Shape()
+			colR, rowR := msReceiver.Shape()
+
+
+			assert.Equal(t, colS, colR)
+			assert.Equal(t, rowS, rowR)
+			i := 0
+			mapS.Range(func(k interface{}, v interface{}) bool {
+				if u, ok := mapR.Load(k); assert.True(t, ok) {
+					assert.Equalf(t, v, u, "S(%d, %d); R(%d, %d)", i/colS, i%colS, i/colR, i%colR)
+				}
+				i += 1
+				return true
+			})
+
+			for i := 0; i < len(ssS); i += 1 {
+				assert.Equalf(t, ssS[i], ssR[i], "S(%d, %d); R(%d, %d)", i/colS, i%colS, i/colR, i%colR)
+			}
+
+			wsSender.ws.Close()
+			wsReceiver.ws.Close()
+		}
+	}
+}
+
+func goTestWorkFlowNoInterrupt(t *testing.T, pause time.Duration) {
+	testWsNum := 20
+	initRow, initCol := 100, 100
+	tokenS, tokenR := login(t, loginParams[0]), login(t, loginParams[1])
+	newSheetArg := utils.NewSheetParams{
+		Token: tokenS,
+		Name:  "test",
+		InitRows: uint(initRow),
+		InitColumns: uint(initCol),
+	}
+	raw, _ := getPostRaw(randomHostHttp(), "newsheet", newSheetArg)
+	fidInt, err := strconv.Atoi(string(raw))
+	fid := uint(fidInt)
+	if assert.NoError(t, err) {
+		getSheetArg := utils.GetSheetParams{
+			Token: tokenR,
+			Fid: fid,
+		}
+		getSheetRet := GetSheetRet{}
+		err = getPostRet(randomHostHttp(), "getsheet", getSheetArg, &getSheetRet)
+		if assert.NoError(t, err) {
+			assert.EqualValues(t, fid, getSheetRet.Fid)
+			assert.EqualValues(t, 0, getSheetRet.CheckPointNum)
+			assert.Equal(t, "test", getSheetRet.Name)
+
+			row, col := len(getSheetRet.Content) / getSheetRet.Columns, getSheetRet.Columns
+			assert.Equal(t, len(getSheetRet.Content), row * col)
+
+			ms := make([]*cache.MemSheet, testWsNum)
+			for i := 0; i < testWsNum; i += 1 {
+				ms[i] = cache.NewMemSheet(initRow, initCol)
+			}
+
+			var msSender, msReceiver *cache.MemSheet
+			var mapS, mapR sync.Map
+
+			type cellKey struct {
+				Row int
+				Col int
+			}
+
+			cnt := 0
+			onAcquireS := func(msg sheetPrepareNotify) {}
+			onModifyS := func(msg sheetModifyNotify) {
+				logger.Info(cnt)
+				cnt++
+				logger.Debugf("[%+v] onModify", msg)
+				msSender.Set(msg.Row, msg.Col, msg.Content)
+				mapS.Store(cellKey{Row: msg.Row, Col: msg.Col}, msg.Content)
+			}
+			onReleaseS := func(msg sheetPrepareNotify) {}
+			onConnS := func(msg sheetOnConnNotify) {
+				logger.Debugf("[%+v] onConnection", msg)
+				msSender = cache.NewMemSheetFromStringSlice(msg.Content, msg.Columns)
+				for i := 0; i < len(msg.Content); i += 1 {
+					mapS.Store(cellKey{Row: i/msg.Columns, Col: i%msg.Columns}, msg.Content[i])
+				}
+			}
+
+			onAcquireR := func(msg sheetPrepareNotify) {}
+			onModifyR := func(msg sheetModifyNotify) {
+				logger.Debugf("[%+v] onModify", msg)
+				msReceiver.Set(msg.Row, msg.Col, msg.Content)
+				mapR.Store(cellKey{Row: msg.Row, Col: msg.Col}, msg.Content)
+			}
+			onReleaseR := func(msg sheetPrepareNotify) {}
+			onConnR := func(msg sheetOnConnNotify) {
+				logger.Debugf("[%+v] onConnection", msg)
+				msReceiver = cache.NewMemSheetFromStringSlice(msg.Content, msg.Columns)
+				for i := 0; i < len(msg.Content); i += 1 {
+					mapR.Store(cellKey{Row: i/msg.Columns, Col: i%msg.Columns}, msg.Content[i])
+				}
+			}
+			wsUrlS, wsUrlR := getWSAddr(tokenS, fid), getWSAddr(tokenR, fid)
+			wsSender := NewWebSocket(t, wsUrlS, onAcquireS, onModifyS, onReleaseS, onConnS)
+			wsReceiver := NewWebSocket(t, wsUrlR, onAcquireR, onModifyR, onReleaseR, onConnR)
+
+
+			runWS := func(ws *myWS, stopChan chan int) {
+				for {
+					select {
+					case <- stopChan:
+						return
+					default:
+						row, col := rand.Int()%100, rand.Int()%100
+						content := strings.Repeat("test", rand.Int()%30)
+
+						err := ws.SendJson("modify", sheetModifyMessage{
+							Row:     row,
+							Col:     col,
+							Content: content,
+							Info:    nil,
+						})
+
+						if !assert.NoError(t, err) {
+							break
+						}
+						time.Sleep(pause)
+					}
+				}
+			}
+			stopChanS, stopChanR := make(chan int), make(chan int)
+			go runWS(wsSender, stopChanS)
+			go runWS(wsReceiver, stopChanR)
+			time.Sleep(30 * time.Second)
+			stopChanS <- 1
+			stopChanR <- 1
+			time.Sleep(15 * time.Second)
+			ssS, ssR := msSender.ToStringSlice(), msReceiver.ToStringSlice()
+			colS, rowS := msSender.Shape()
+			colR, rowR := msReceiver.Shape()
+
+
+			assert.Equal(t, colS, colR)
+			assert.Equal(t, rowS, rowR)
+			i := 0
+			mapS.Range(func(k interface{}, v interface{}) bool {
+				if u, ok := mapR.Load(k); assert.True(t, ok) {
+					assert.Equalf(t, v, u, "S(%d, %d); R(%d, %d)", i/colS, i%colS, i/colR, i%colR)
+				}
+				i += 1
+				return true
+			})
+
+			for i := 0; i < len(ssS); i += 1 {
+				assert.Equalf(t, ssS[i], ssR[i], "S(%d, %d); R(%d, %d)", i/colS, i%colS, i/colR, i%colR)
+			}
+
+			wsSender.ws.Close()
+			wsReceiver.ws.Close()
+		}
+	}
 }
