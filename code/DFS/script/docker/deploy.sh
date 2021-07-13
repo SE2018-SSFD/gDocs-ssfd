2083dcc0f579ffae8b6d4da8f6b4b8e46ff0d8d1
mokkkkk <55308586+mokkkkk@users.noreply.github.com>
2021-07-13 19:27:01 +0800

    fix:modify deploy

--- code/DFS/script/docker/deploy.sh
+++ code/DFS/script/docker/deploy.sh
@@ -1,34 +1,13 @@
-MASTER1PORT=1234
-MASTER2PORT=1235
-MASTER3PORT=1236
-MASTER1DIR=../log/log_1
-MASTER2DIR=../log/log_2
-MASTER3DIR=../log/log_3
-CHUNKSERVERNUM=3
-CHUNKSERVERROOT=../data/ck
-rm -rf ${CHUNKSERVERROOT}
-mkdir -p ${CHUNKSERVERROOT}
-rm -rf ${MASTER1DIR}
-mkdir ${MASTER1DIR}
-rm -rf ${MASTER2DIR}
-mkdir ${MASTER2DIR}
-rm -rf ${MASTER3DIR}
-mkdir ${MASTER3DIR}
-
-docker build -f ./Dockerfile ../.. -t dfsnode
-for a in `eval echo {3000..$[CHUNKSERVERNUM+3000]}`
-do
-mkdir ${CHUNKSERVERROOT}/ck"${a}"
-docker run --expose=${a} --name=chunkServer${a} dfsnode ./NodeRunner chunkServer 0.0.0.0:"${a}" ${CHUNKSERVERROOT}/ck"${a}" ${MASTER1PORT} &> ../log/chunkServerOutput"${a}".log &
+r${a} dfsnode ./NodeRunner chunkServer 0.0.0.0:"${a}" ck"${a}" ${MASTER1PORT} &
 done
 sleep 2
-docker run --expose=${MASTER1PORT} --name=master1 dfsnode ./NodeRunner master ${MASTER1PORT} ${MASTER1DIR}  &> ../log/master1Output.log &
-docker run --expose=${MASTER2PORT} --name=master2 dfsnode ./NodeRunner master ${MASTER2PORT} ${MASTER2DIR}  &> ../log/master2Output.log &
-docker run --expose=${MASTER3PORT} --name=master3 dfsnode ./NodeRunner master ${MASTER3PORT} ${MASTER3DIR}  &> ../log/master3Output.log &
+docker run --expose=${MASTER1PORT} --name=master1 dfsnode ./NodeRunner master 0.0.0.0:${MASTER1PORT} ${MASTER1DIR} &
+docker run --expose=${MASTER2PORT} --name=master2 dfsnode ./NodeRunner master 0.0.0.0:${MASTER2PORT} ${MASTER2DIR} &
+docker run --expose=${MASTER3PORT} --name=master3 dfsnode ./NodeRunner master 0.0.0.0:${MASTER3PORT} ${MASTER3DIR} &
 
 CLIENTPORT1=1333
 CLIENTPORT2=1334
 CLIENTPORT3=1335
-docker run --expose=${CLIENTPORT1} --name=client1 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT1} ${MASTER1PORT} &> ../log/clientOutput${CLIENTPORT1}.log &
-docker run --expose=${CLIENTPORT2} --name=client2 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT2} ${MASTER1PORT} &> ../log/clientOutput${CLIENTPORT2}.log &
-docker run --expose=${CLIENTPORT3} --name=client3 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT3} ${MASTER1PORT} &> ../log/clientOutput${CLIENTPORT3}.log &
+docker run --expose=${CLIENTPORT1} --name=client1 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT1} ${MASTER1PORT} &
+docker run --expose=${CLIENTPORT2} --name=client2 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT2} ${MASTER1PORT} &
+docker run --expose=${CLIENTPORT3} --name=client3 dfsnode ./NodeRunner client 0.0.0.0:${CLIENTPORT3} ${MASTER1PORT} &
